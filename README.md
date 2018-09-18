ef6fc55bb4271947db837f06650545bdcec70f83
super <zxpscau@163.com>
2018-09-18 16:50:48 +0800

    update

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 
 ### **个人小项目展示及源码小仓库。**
 
-1. **VUE 时间选择器**
+8. **VUE 时间选择器**
 - [相关文章——vue 手写一个时间选择器](https://juejin.im/post/5b62b0cfe51d453489494efb)
   
 - [源码](./datepicker/Datepicker.vue)+[Demo](https://zxpsuper.github.io/Demo/datepicker/index.html)
 ---
-2. **CANVAS 刮奖小 Demo**
+7. **CANVAS 刮奖小 Demo**
 - [相关文章——canvas 实现刮刮乐](https://blog.csdn.net/weixin_38788347/article/details/78239704)
   
 - [源码](./guajiang/index.html)+[Demo](https://zxpsuper.github.io/Demo/guajiang/index.html)
 ---
-3. **瀑布流的实现**
+6. **瀑布流的实现**
 - [相关文章——瀑布流布局的实现](https://blog.csdn.net/weixin_38788347/article/details/78390064)
 
 - CSS 实现瀑布流——[源码](./waterflow/waterfallcss.html)+[Demo](https://zxpsuper.github.io/Demo/waterflow/waterfallcss.html)
@@ -21,28 +21,28 @@
 
 - JQUERY 实现瀑布流——[源码](./waterflow/waterfalljq.html)+[Demo](https://zxpsuper.github.io/Demo/waterflow/waterfalljq.html)
 ---
-4. **websocket**
+5. **websocket**
 
 - [相关文章——使用 socket.io.js 实现 websocket 实时通讯](https://blog.csdn.net/weixin_38788347/article/details/79726992)
   
 - [源码](./websocket/)
 ---
-5. **图片懒加载**
+4. **图片懒加载**
 - [相关文章——懒加载](https://blog.csdn.net/weixin_38788347/article/details/78217372)
   
 - [源码](./lazyload.html)+[Demo](https://zxpsuper.github.io/Demo/lazyload.html)
 ---
-6. **拖曳效果**
+3. **拖曳效果**
 - [相关文章——利用JQ实现简单的拖曳效果](https://blog.csdn.net/weixin_38788347/article/details/78273565)
   
 - [源码](./drag.html)+[Demo](https://zxpsuper.github.io/Demo/drag.html)
 ---
-7. **Quill 富文本编辑器**
+2. **Quill 富文本编辑器**
 - [相关文章——quill——简单的富文本编辑器](https://blog.csdn.net/weixin_38788347/article/details/78249433)
   
 - [源码](./quill.html)+[Demo](https://zxpsuper.github.io/Demo/quill.html)
 ---
-8. **CANVAS 页面时钟**
+1. **CANVAS 页面时钟**
 - [相关文章](https://blog.csdn.net/weixin_38788347/article/details/78239704)
   
 - [源码](./watch.html)+[Demo](https://zxpsuper.github.io/Demo/watch.html)
--- color/index.js
+++ color/index.js
@@ -1,98 +1,140 @@
-class Game {
+class ColorGame {
   constructor(userOption) {
     this.option = {
-      time: 30,
-      level: 'normal',
-      end: () => {}
+      time: 30, // 总时长
+      end: score => {
+        document.getElementById(
+          "screen"
+        ).innerHTML = `<div class="result" style="width: 100%;">
+        <div class="block-inner" style="background-color: red" id="restart"> You score is ${score} , click to start again</div>
+      </div>`;
+        addEvent(document.getElementById("restart"), "click", () => {
+          this.init();
+        });
+      } // 结束函数
     };
-    this.step = 0;
-    this.score = 0;
-    this.init(userOption);
+    this.init(userOption); // 初始化，合并用户配置
   }
   init(userOption) {
-    // 合并用户配置
+    this.step = 0;
+    this.score = 0;
     if (Object.assign) {
       Object.assign(this.option, userOption);
     } else {
-      this.extend(this.option, userOption, true);
+      extend(this.option, userOption, true);
     }
-
+    // 倒计时赋值
+    this.time = this.option.time;
     // 设置初始时间和分数
-    document.getElementsByClassName('wgt-score')[0].innerHTML = `得分：<span id="score">${this.score}</span>
-    时间：<span id="timer">${this.option.time}</span>`
+    document.getElementsByClassName(
+      "wgt-score"
+    )[0].innerHTML = `得分：<span id="score">${this.score}</span>
+    时间：<span id="timer">${this.time}</span>`;
 
     // 开始计时
     window.timer = setInterval(() => {
-      if (this.option.time === 0) {
-        clearInterval(window.timer)
-        this.option.end(this.score)
+      if (this.time === 0) {
+        clearInterval(window.timer);
+        this.option.end(this.score);
       } else {
-        this.option.time--
-        document.getElementById('timer').innerHTML = this.option.time
+        this.time--;
+        document.getElementById("timer").innerHTML = this.time;
       }
-    }, 1000)
-    this.nextStep()
-    
+    }, 1000);
+    this.nextStep(); // 下一关
   }
   nextStep() {
     // 记级
-    this.step++ 
-    let col // 列数
+    this.step++;
+    let col; // 列数
     if (this.step < 6) {
-      col = this.step + 1
+      col = this.step + 1;
     } else if (this.step < 12) {
-      col = Math.floor(this.step / 2) * 2
-    } else if (this.step < 18){
-      col = Math.floor(this.step / 3) * 3
+      col = Math.floor(this.step / 2) * 2;
+    } else if (this.step < 18) {
+      col = Math.floor(this.step / 3) * 3;
     } else {
-      col = 16
+      col = 16;
     }
     // 小盒子宽度
-    let blockWidth = ((100/col).toFixed(2) * 100 - 1) / 100
-    let randomBlock = Math.floor(col*col*Math.random())
-    let [normalColor, specialColor] = getColor(this.step)
+    let blockWidth = ((100 / col).toFixed(2) * 100 - 1) / 100;
+    // 随机盒子index
+    let randomBlock = Math.floor(col * col * Math.random());
+    // 解构赋值获取一般颜色和特殊颜色
+    let [normalColor, specialColor] = getColor(this.step);
+    // 模板字符串
     let item = `<div class="block" style="width: ${blockWidth}%;">
     <div class="block-inner" style="background-color: #${normalColor}"></div>
-  </div>`
-    let arr = []
-    for (let i = 0; i < col*col; i++) {
-      arr.push(item)
-    }
+  </div>`;
+    // 包含所有盒子的数组
+    let arr = [];
+
+    for (let i = 0; i < col * col; i++) arr.push(item);
+    // 修改随机盒子
     arr[randomBlock] = `<div class="block" style="width: ${blockWidth}%;">
     <div class="block-inner" style="background-color: #${specialColor}" id="special-block"></div>
-  </div>`
-    document.getElementById('screen').innerHTML = arr.join('')
-    document.getElementById('special-block').addEventListener('click', () => { 
-      this.nextStep()
-      this.score ++ 
-      document.getElementById('score').innerHTML = this.score
-    })
-  }
-  // 合并参数方法
-  extend(o, n, override) {
-    for (var p in n) {
-      if (n.hasOwnProperty(p) && (!o.hasOwnProperty(p) || override))
-        o[p] = n[p];
-    }
+  </div>`;
+    document.getElementById("screen").innerHTML = arr.join("");
+    // 监听特殊盒子点击事件
+    addEvent(document.getElementById("special-block"), "click", () => {
+      this.nextStep();
+      this.score++;
+      document.getElementById("score").innerHTML = this.score;
+    });
   }
 }
+// 合并参数方法
+function extend(o, n, override) {
+  for (var p in n) {
+    if (n.hasOwnProperty(p) && (!o.hasOwnProperty(p) || override))
+      o[p] = n[p];
+  }
+}
+// 根据关卡等级返回相应的一般颜色和特殊颜色
 function getColor(step) {
-  let random = Math.floor(Math.abs(100-4*step))
-  var color = randomColor(0, 255), m = color.match(/[\da-z]{2}/g);
-  for (var i = 0; i < m.length; i++) m[i] = parseInt(m[i], 16);//rgb
-  let specialColor = Math.floor(m[0] + (Math.random() < 0.5 ? -1 : 1) * random).toString(16) +
-  Math.floor(m[1] + (Math.random() < 0.5 ? -1 : 1) * random).toString(16) +
-  Math.floor(m[2] + (Math.random() < 0.5 ? -1 : 1) * random).toString(16);
-  return [color, specialColor]
+  let random = Math.floor(100/step);
+  // let random = Math.floor(Math.abs(100 - 4 * step));
+  let color = randomColor(17, 255),
+    m = color.match(/[\da-z]{2}/g);
+  for (let i = 0; i < m.length; i++) m[i] = parseInt(m[i], 16); //rgb
+  let specialColor =
+    getRandomColorNumber(m[0], random) +
+    getRandomColorNumber(m[1], random) +
+    getRandomColorNumber(m[2], random);
+  return [color, specialColor];
 }
 
+function getRandomColorNumber(num, random) {
+  let temp = Math.floor(num + (Math.random() < 0.5 ? -1 : 1) * random);
+  if (temp > 255) {
+    return "ff";
+  } else if (temp > 16) {
+    return temp.toString(16);
+  } else if (temp > 0) {
+    return "0" + temp.toString(16);
+  } else {
+    return "00";
+  }
+}
+// 随机颜色 min 大于16
 function randomColor(min, max) {
-  var r = randomNum(min, max).toString(16)
-  var g = randomNum(min, max).toString(16)
-  var b = randomNum(min, max).toString(16)
-  return r + g + b
+  var r = randomNum(min, max).toString(16);
+  var g = randomNum(min, max).toString(16);
+  var b = randomNum(min, max).toString(16);
+  return r + g + b;
+}
+// 随机数
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min) + min)
+// 事件兼容方法
+function addEvent(element, type, handler) {
+  if (element.addEventListener) {
+    element.addEventListener(type, handler, false);
+  } else if (element.attachEvent) {
+    element.attachEvent("on" + type, handler);
+  } else {
+    element["on" + type] = handler;
+  }
 }
