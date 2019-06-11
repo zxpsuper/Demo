39e306c2e39f2cf9c3af7d5e1e9a1e94c3297279
suporka <zxpscau@163.com>
2019-06-11 14:20:40 +0800

    更新一个测试文件夹

---
+++ test/precache-manifest.229692a318db954d982f728e0631bf95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "css/2.9fe235477c54bcd92b7b.css"
+  },
+  {
+    "url": "css/main.9fe235477c54bcd92b7b.css"
+  },
+  {
+    "url": "imgs/8bd9acf9afd98ffe0c46e2e2a14af831.png"
+  },
+  {
+    "url": "imgs/eaca5e287745bdae35a60ddb33dcc195.png"
+  },
+  {
+    "revision": "40aa1f348f6148eae3405060763c9f4c",
+    "url": "index.html"
+  },
+  {
+    "revision": "7fa6c6ea84920d7ada86",
+    "url": "js/2.0176f593467e469c69a7.js"
+  },
+  {
+    "revision": "33eea57bc0484833dcce",
+    "url": "js/main.1bf183906865f78df721.js"
+  },
+  {
+    "revision": "6bce77df75edd3d11adf",
+    "url": "js/vendor.e08b111d438e8853c8de.js"
+  },
+  {
+    "revision": "c451d1e610029a9e13d57859a594a733",
+    "url": "static\\head.json"
+  }
+]);
